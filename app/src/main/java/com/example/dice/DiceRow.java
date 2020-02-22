b85828154d86f4af8aba5760a10e9a6b0e7a94d2
schemabuoi <charyszczakg@gmail.com>
2020-02-22 17:53:00 +0100

    Update DiceRow.java

--- app/src/main/java/com/example/dice/DiceRow.java
+++ app/src/main/java/com/example/dice/DiceRow.java
@@ -7,9 +7,12 @@ import android.widget.LinearLayout;
 
 public class DiceRow extends LinearLayout {
 
+    private Context ct;
+
     public DiceRow(Context ct)
     {
         super(ct);
+        this.ct = ct;
         setVisualProperties();
     }
 
@@ -23,9 +26,24 @@ public class DiceRow extends LinearLayout {
         setGravity(Gravity.CENTER);
     }
 
-    public void addDice(Dice dice)
+    public void addDice()
     {
+        Dice dice = new Dice(ct);
         addView(dice);
+    }
+
+    public void setDiceSizes(Dice.DiceSize size)
+    {
+        for(int i = 0; i < getChildCount(); i++)
+        {
+            Dice dice = (Dice) getChildAt(i);
+            dice.setDiceSize(size);
+        }
+    }
+
+    public void removeDice()
+    {
+
     }
 
 }
